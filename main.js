293c956668b0c7267e19f3c8166cff3c65ff5819
Fahad Hossain <evilinfinite7@gmail.com>
2018-02-03 18:23:30 +0600

    add typescript support

--- main.js
+++ main.js
@@ -7,38 +7,46 @@ const online = require('is-online')
 const { promise: ora } = require('ora')
 const kebab = require('lodash.kebabcase')
 const isYarn = require('@danielbayerlein/caniuse-yarn')()
-const { argv: { name } } = yargs
+const { argv: { name, ts } } = yargs
   .command(
     ['create <name>', '$0 <name>'],
     'Create a new React + Parcel project',
     y => y.positional('name', { describe: 'project name', type: 'string' })
   )
+  .options('ts', {
+    alias: 'typescript',
+    type: 'boolean',
+    default: false,
+    describe: 'use typescript'
+  })
   .help()
 
 const getStarted = yarn =>
   `To start:
-  cd ${name}
-  ${yarn ? 'yarn' : 'npm'} start
+
+cd ${name}
+${yarn ? 'yarn' : 'npm'} start
 `
 
-const projDir = path.resolve(name)
-const tempDir = path.resolve('react-parcel-project-master')
+const link = ts ? 'https://git.io/vNApC' : 'https://git.io/vNgqK'
+const branchName = ts ? 'typescript' : 'master'
 
-const downloadAndExtract = online => {
+const projDir = path.resolve(name)
+const tempDir = path.resolve('react-parcel-project-' + branchName)
+
+const downloadAndExtract = (archive, url, online) => {
   if (online) {
     const dl = require('download')
-    return dl('https://git.io/vNgqK', '.', { extract: true })
+    return dl(url, '.', { extract: true })
   }
   const dc = require('decompress')
-  return dc(path.resolve(__dirname, 'assets/master.tar.gz'), '.')
+  return dc(path.resolve(__dirname, `assets/${archive}.tar.gz`), '.')
 }
 const renameFolder = (from, to) => fs.move(from, to, { overwrite: true })
 const renameProj = dir => {
   const file = path.join(dir, 'package.json')
   const pkg = Object.assign({}, require(file), {
-    name: kebab(name),
-    author: '',
-    repository: ''
+    name: kebab(name)
   })
   return fs.writeJSON(file, pkg, { spaces: 4 })
 }
@@ -46,7 +54,7 @@ const renameProj = dir => {
 async function run (dir, temp, yarn = false) {
   const text = 'Getting boilerplate'
   const spin = ora(
-    downloadAndExtract(await online())
+    downloadAndExtract(branchName, link, await online())
       .then(_ => renameFolder(temp, dir))
       .then(_ => {
         spin.text = 'Renaming boilerplate'
@@ -63,7 +71,7 @@ async function run (dir, temp, yarn = false) {
       .then(_ => {
         const box = require('boxen')
         console.log('\n\n')
-        console.log(box(getStarted(yarn), { padding: 5 }))
+        console.log(box(getStarted(yarn), { padding: 2 }))
       })
       .then(_ => {
         spin.text = 'Done!'
